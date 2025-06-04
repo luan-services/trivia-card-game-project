d6d04394c0d63f36483a680ab20c6fedff62c986
luan-services <luandotservices@gmail.com>
2025-06-04 17:07:55 -0300

    toggle-player-menu-changes

--- components/game_screen/players_nav_bar/PlayerNavBar.jsx
+++ components/game_screen/players_nav_bar/PlayerNavBar.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { useState } from 'react';
 import { AnimatePresence, scale } from 'motion/react'
 import { motion } from "motion/react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-regular-svg-icons'
+import { faXmark } from '@fortawesome/free-solid-svg-icons'
 
 const PlayerNavBar = ({playerArray}) => {
 
@@ -16,16 +19,19 @@ const PlayerNavBar = ({playerArray}) => {
         <div className="bg-amber-100 container z-10 fixed top-4 left-4  max-w-72 flex flex-col">
             {/* esse motion.div da toggle na animação de movimento para a direita, 
             o pai precisa ter as classes justify-end e start baseadas num useState, 
-            o filho precisa ser o motion object e precisa ter a função transitin */}
-            <div className={`bg-blue-100 flex ${isOpen ? "justify-end" : "justify-start"}`}>
-                <AnimatePresence>
-                    <motion.button  layout transition={{type: "spring", visualDuration: 0.5, bounce: 0.1,
-                }}  className="flex bg-amber-600" onClick={handleToggle}>CLICK</motion.button>
+            o filho precisa ser o motion object e precisa ter a função transition */}
 
+            {/*toggle menu */}
+            <div className={`bg-blue-100 p-1 rounded-lg flex ${isOpen ? "justify-end" : "justify-start"}`}>
+                <AnimatePresence>
+                    <motion.button  layout transition={{type: "spring", visualDuration: 0.5, bounce: 0.1,}}  
+                    className=" rounded-xl p-2 pl-4 pr-4 flex bg-amber-600" onClick={handleToggle}>
+                        <FontAwesomeIcon icon={isOpen ? faXmark : faUser} />
+                    </motion.button>
                 </AnimatePresence>
             </div>
 
-            {/* this motion.div toggles a dropdown menu */}
+            {/* this motion.div is a dropdown menu */}
             <AnimatePresence>
                 {isOpen && 
                     <motion.div initial={{scaleY: 0}} animate={{scaleY: 1}} exit={{scaleY: 0}} transition={{duration: 0.1}} className="origin-top">
